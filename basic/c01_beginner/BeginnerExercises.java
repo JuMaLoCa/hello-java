257d863c4a933c61ea504ba6a39aaa0056f75c90
Juan López <lopezcancio@gmail.com>
2025-08-02 13:37:30 +0200

    Update BeginnerExercises.java

--- basic/c01_beginner/BeginnerExercises.java
+++ basic/c01_beginner/BeginnerExercises.java
@@ -1,32 +1,63 @@
 package basic.c01_beginner;
 
-/*
-Clase 17 - Ejercicios: Variables y constantes
-Vídeo: https://youtu.be/JOAqpdM36wI?t=6732
-*/
+	/*
+	Clase 17 - Ejercicios: Variables y constantes
+	Vídeo: https://youtu.be/JOAqpdM36wI?t=6732
+	*/
 
-public class BeginnerExercises {
+	public class BeginnerExercises {
 
-    public static void main(String[] args) {
+	    public static void main(String[] args) {
 
-        // 1. Declara una variable de tipo String y asígnale tu nombre.
+	        // 1. Declara una variable de tipo String y asígnale tu nombre.
+	    	String nombre = "Juan";
+	    	System.out.println(nombre);
 
-        // 2. Crea una variable de tipo int y asígnale tu edad.
+	        // 2. Crea una variable de tipo int y asígnale tu edad.
+	    	int edad = 52;
+	    	System.out.println(edad);
 
-        // 3. Crea una variable double con tu altura en metros.
+	        // 3. Crea una variable double con tu altura en metros.
+	    	double altura = 1.77;
+	    	System.out.println(altura);
 
-        // 4. Declara una variable de tipo boolean que indique si te gusta programar.
+	        // 4. Declara una variable de tipo boolean que indique si te gusta programar.
+	    	boolean meGustaProgramar = true;
+	    	System.out.println("¿Me gusta programar? " + meGustaProgramar);
 
-        // 5. Declara una constante con tu email.
+	        // 5. Declara una constante con tu email.
+	    	final String email = "lopezcancio@gmail.com";
+	    	System.out.println(email);
 
-        // 6. Crea una variable de tipo char y guárdale tu inicial.
+	        // 6. Crea una variable de tipo char y guárdale tu inicial.
+	    	char inicial = 'J';
+	    	System.out.println(inicial);
+	    	
 
-        // 7. Declara una variable de tipo String con tu localidad, y a continuación cambia su valor y vuelve a imprimirla.
+	        // 7. Declara una variable de tipo String con tu localidad, y a continuación cambia su valor y vuelve a imprimirla.
+	    	String localidad = "Siero";
+	    	System.out.println(localidad);
+	    	localidad = "Oviedo";
+	    	System.out.println(localidad);
 
-        // 8. Crea una variable int llamada a, otra b, e imprime la suma de ambas.
+	        // 8. Crea una variable int llamada a, otra b, e imprime la suma de ambas.
+	    	int a = 8;
+	    	int b = 7;
+	    	System.out.println("El resultado de la suma es: "+(a+b));
 
-        // 9. Imprime el tipo de dos variables creadas anteriormente.
+	        // 9. Imprime el tipo de dos variables creadas anteriormente.
+	    	Character caracter = 'J';
+	    	Integer integer = 25;
+	    	System.out.println("Tipo de 'localidad' es: "+localidad.getClass().getSimpleName());
+	    	System.out.println("Tipo de 'caracter' es: "+caracter.getClass().getSimpleName());
+	    	System.out.println("Tipo de 'integer' es: "+integer.getClass().getSimpleName());
+	    	// System.out.println("Tipo de 'a' es: "+a.getClass().getSimpleName());  // De los tipos de datos "primitivos" no se puede obtener el tipo.
 
-        // 10. Intenta declarar una variable sin inicializarla y luego asígnale un valor antes de imprimirla.
-    }
+	        // 10. Intenta declarar una variable sin inicializarla y luego asígnale un valor antes de imprimirla.
+	    	String variableSinValor;
+	    	variableSinValor = "valorVariableSinValor";
+	    	System.out.println("Este es el valor de la variableSinValor: "+variableSinValor);
+	    		
+	    }
+	
 }
